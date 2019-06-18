9b7cd55170fffe151501f2aea24e24387e328fed
composure <xutong1107@gmail.com>
2019-06-18 20:28:42 +0800

    2019-06-18

--- src/class.js
+++ src/class.js
@@ -1,22 +1,33 @@
-class Person{
-    constructor(name,age){
-        this.name=name
-        this.age=age
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
     }
-    say(){
+    say() {
         console.log(`I'm ${this.name}`)
     }
 }
 
-class Child extends Person{
-    constructor(name,age,id){
-        super(name,age) //如果一个子类，通关关键字extends继承父类，在子类的constructor必须优先调用super()
+class Child extends Person {
+    constructor(name, age, id) {
+        super(name, age) //如果一个子类，通关关键字extends继承父类，在子类的constructor必须优先调用super()
         // super()是一个函数，是父类constructor的一个引用
-        this.id=id
+        this.id = id
     }
 }
-const child=new Child('张三',18,'hahah')
+const child = new Child('张三', 18, 'hahah')
 
 console.log(child)
 
 
+class A {
+    static hello() {
+        console.log('hello world');
+    }
+}
+
+class B extends A {
+
+}
+
+var  s=new B()
