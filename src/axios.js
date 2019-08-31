2b26fe3551c66feb001b82643dd5453e3c00d8d3
composure <xutong1107@gmail.com>
2019-08-31 15:48:39 +0800

    feat:add easy mock

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 //node 环境
 const axios=require('axios')
 const {log}=console
-const url='https://sm.ms/api/list'
+// const url='https://sm.ms/api/list'
+const url='https://www.easy-mock.com/mock/5aae56af9593e16999e1934d/example/query'
 
 
 axios({
