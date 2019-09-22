815a30b1b72a5862d4ad0569148718fddb2f80ac
composure <xutong1107@gmail.com>
2019-09-22 11:34:53 +0800

    feat(nodejs实战):剪刀石头小游戏

---
+++ doc/nodejs.js
@@ -0,0 +1,25 @@
+const {log}=console
+// 获取nodejs（第一个）输入的变量
+const userAction = process.argv[process.argv.length - 1]
+// 计算机随机出一个
+let random = Math.random() * 3
+let computerAction;
+const rock='rock',paper='paper',scissor='scissor';
+if (random < 1) {
+  computerAction = rock
+} else if (random > 2) {
+  computerAction = paper
+} else {
+  computerAction =scissor
+}
+if(userAction===computerAction){
+    log('平局！！')
+}else if(
+  (userAction===rock&&computerAction==scissor)||
+  (userAction===paper&&computerAction==rock)||
+  (userAction===scissor&&computerAction==paper)
+){
+  log('你赢了！！')
+}else{
+  log('你输了！！')
+}
---
+++ doc/nodejs.md
@@ -0,0 +1,5 @@
+### 简介
+#### 1. Node环境的js和Chrome环境的js运行的不同点
++ Nodejs没有浏览器的API，如document、window等
++ 加入了许多NodeJS的API
++ js控制浏览器，NodeJS控制计算机
