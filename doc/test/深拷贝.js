1b7213a3d6f0702a0d966c1d06064cf6250153c0
composure <xutong1107@gmail.com>
2020-03-10 22:23:13 +0800

    update:nodejs-http

---
+++ doc/test/深拷贝.js
@@ -0,0 +1,35 @@
+//  JSON 版本 
+const newObj = (someObj)=>JSON.parse(JSON.stringify(someObj))
+
+
+const deepCopy = (obj)=>{
+  // 判断类型
+  const type = typeof obj 
+  if(type === 'object'){
+    const result = obj.construstor == Array ? [] :{}
+    for(let i in obj){
+      result[i] = typeof obj[i] === 'object' ? deepCopy(obj[i]) : obj[i]
+    }
+    return result
+  }else{
+    result = obj
+  }
+  return result
+}
+
+var obj = {
+  a:{
+    aa:{
+      aaa:123,
+      bbbb:function (s) {return  s},
+      ccc:/^[0-9]$/
+    }
+  },
+  b:{bb:1}
+}
+
+var result = deepCopy(obj)
+result.test = 124
+console.log(obj)
+console.log(result)
+console.log(result.a.aa.bbbb(12))
