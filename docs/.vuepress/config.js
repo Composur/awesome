c98cafededf230c1de4c5e29bc327ddd09e9f301
composure <xutong1107@gmail.com>
2020-04-28 22:45:42 +0800

    test:vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+var nav = require('./links/nav.js');
+module.exports = {
+  base: '/awesome/',
+  title: 'Awesome-FE',
+  description: '个人笔记',
+  themeConfig: {
+    nav,
+  },
+};
---
+++ docs/.vuepress/links/nav.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    text: 'Awesomes',
+    link: '/react-hooks',
+  },
+  {
+    text: '博客',
+    link: '/nodejs',
+  },
+  {
+    text: '笔记',
+    link: '/notes',
+  },
+  {
+    text: '书籍',
+    link: '/books',
+  },
+  {
+    text: '书签',
+    link: '/bookmarks',
+  },
+  {
+    text: '面试',
+    link: '/interviews',
+  },
+  {
+    text: '最近阅读',
+    link: 'recently',
+  },
+  {
+    text: 'GitHub',
+    link: 'https://github.com/ziyi2/awesome',
+  },
+];
