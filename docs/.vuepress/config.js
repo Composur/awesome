6e3236111cccc1440a90aa5f7788ac742e2c07f4
composure <xutong1107@gmail.com>
2020-04-29 09:44:12 +0800

    test:vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,10 @@
 var nav = require('./links/nav.js');
 module.exports = {
   base: '/awesome/',
+  dest: './dist',  
   title: 'Awesome-FE',
   description: '个人笔记',
+  repo: 'https://github.com/Composur/awesome', // 添加 github 链接
   themeConfig: {
     nav,
   },
