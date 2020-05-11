7b27f1d2778199d5b6fa728d44b143ad2fa84b9e
composure <xutong1107@gmail.com>
2020-05-11 22:03:40 +0800

    update

--- src/test/getURL.js
+++ src/test/getURL.js
@@ -5,11 +5,11 @@ function parse_url(_url) {
   //定义函数
   var pattern = /(\w+)=(\w+)/gi; //定义正则表达式
   var parames = {}; //定义数组
-  url.replace(pattern, function (a, b, c) {
+  _url.replace(pattern, function (a, b, c) { // k=value,k,value
     parames[b] = c;
   });
   return parames; //返回这个数组.
 }
 
 var url = "http://www.baidu.com?name=elephant&age=25&sex=male";
-var params = parse_url(url); // ["name=elephant", "age=25", "sex=male"]
+console.log(parse_url(url)); // { name: 'elephant', age: '25', sex: 'male' }
