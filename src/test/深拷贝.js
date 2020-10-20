20dbc1a60aad4959ff17a5415b117bc854f97ea9
composure <xutong1107@gmail.com>
2020-10-20 08:58:00 +0800

    docs: http

--- src/test/深拷贝.js
+++ src/test/深拷贝.js
@@ -1,34 +1,59 @@
 //  JSON 版本 
-const newObj = (someObj)=>JSON.parse(JSON.stringify(someObj))
+const newObj = (someObj) => JSON.parse(JSON.stringify(someObj))
 
 
-const deepCopy = (obj)=>{
-  // 判断类型
-  const type = typeof obj 
-  if(type === 'object'){
-    const result = obj.construstor == Array ? [] :{}
-    for(let i in obj){
-      result[i] = typeof obj[i] === 'object' ? deepCopy(obj[i]) : obj[i]
+const deepCopy = (obj) => {
+    // 判断类型
+    const type = typeof obj
+    if (type === 'object') {
+        const result = obj.construstor == Array ? [] : {}
+        for (let i in obj) {
+            result[i] = typeof obj[i] === 'object' ? deepCopy(obj[i]) : obj[i]
+        }
+        return result
+    } else {
+        result = obj
     }
     return result
-  }else{
-    result = obj
-  }
-  return result
 }
 
 var obj = {
-  a:{
-    aa:{
-      aaa:123,
-      bbbb:function (s) {return  s},
-      ccc:/^[0-9]$/
+    a: {
+        aa: {
+            aaa: 123,
+            bbbb: function (s) {
+                return s
+            },
+            ccc: /^[0-9]$/
+        }
+    },
+    b: {
+        bb: [1, 2, 3, 4, {
+            aa: 124
+        }]
     }
-  },
-  b:{bb:1}
 }
 
 var result = deepCopy(obj)
-// result.test = 124
-// console.log(obj)
+result.test = 124
+console.log(obj)
 console.log(result)
+var deepCopyFn = (obj) => {
+    const type = typeof obj
+    // 定义个空对象
+    let res = obj.construstor === Array ? [] : {}
+    if (type === 'object') {
+
+        // 遍历
+        for (let i in obj) {
+            // 赋值
+            // 判断是否是对象是的话递归
+            res[i] = typeof obj[i] === 'object' ? deepCopyFn(obj[i]) : obj[i]
+        }
+        return res; // 定义个空对象
+    } else {
+        return obj
+    }
+
+}
+// console.log(deepCopyFn(obj));
